100afe7c34ad4efdf35b18853029ad9c45e515f6
Ivan Galakhov <igalakhov00@stuy.edu>
2018-04-13 01:50:02 -0400

    small reset to clean

--- userOfMazeSolver.java
+++
@@ -1,8 +0,0 @@
-public class UserOfMazeSolver {
-  public static void main(String[] args){
-      //test maze solving
-      Maze toSolve = new Maze("MAZES/PATH/TO/WHATEVER", 1, 1);
-      MazeSolver mySolver = new MazeSolver();
-      System.out.println("The maze is " + mySolver.solve() ? "solvable!" : "not solvable");
-  }
-}
